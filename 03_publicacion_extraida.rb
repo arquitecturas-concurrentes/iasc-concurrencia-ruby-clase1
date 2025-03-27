b5f0507fb7066ebc60a781e46eed2a9cf02b0529
Ernesto Bossi <bossi.ernestog@gmail.com>
2025-03-27 17:17:51 -0300

    Update 2025c1

--- 03_publicacion_extraida.rb
+++ 03_publicacion_extraida.rb
@@ -11,8 +11,8 @@ end
 100.times.map do
   Thread.new do
     100_000.times do
-      nuevo_valor = valor_del_contador()
-      setear_contador(nuevo_valor + 1 )
+      nuevo_valor = valor_del_contador
+      setear_contador(nuevo_valor + 1)
     end
   end
 end.each(&:join)
--- 07_mapa_no_sincronizado.rb
+++ 07_mapa_no_sincronizado.rb
@@ -1,5 +1,6 @@
 mapa = java.util.HashMap.new
 executor = java.util.concurrent.Executors.newFixedThreadPool(20)
+
 500.times do
   executor.execute { mapa["foo"] = 1 }
   executor.execute { mapa["foo"] = 2 }
